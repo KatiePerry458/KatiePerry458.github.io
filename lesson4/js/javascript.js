c0f5e6d73bdec9cd89878c20e5136c86f9246042
KatiePerry458 <pie11010@byui.edi>
2019-10-14 15:36:04 -0700

    kp

--- lesson4/js/javascript.js
+++ lesson4/js/javascript.js
@@ -1,13 +1,14 @@
 /* Date Stamp*/
-<script type="text/javascript">
 
 var timeStamp = Math.floor(Date.now() / 1000);
 
 var d = new Date();
-</script>
+
+
 
 /* Responsive Menu*/
-
 function toggleMenu(){
+  console.log("hi");
   document.getElementById("navigation").classList.toggle("hide");
 }
+
