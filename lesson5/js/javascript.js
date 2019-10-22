bf089f62e832b8cc55c678ae6f1e7f379e2f300a
KatiePerry458 <pie11010@byui.edi>
2019-10-22 10:43:59 -0700

    kp

--- lesson5/js/javascript.js
+++ lesson5/js/javascript.js
@@ -1,14 +1,18 @@
 /* Date Stamp*/
 
 var timeStamp = Math.floor(Date.now() / 1000);
-
 var d = new Date();
 
 
+/* Banner Announcement*/
+var today = d.getDay();
+if (parseInt == today)
+  case "Friday":
+    $(announcement.style.display = "block").show("hide");
+}
 
 /* Responsive Menu*/
 function toggleMenu(){
-  console.log("hi");
   document.getElementById("navigation").classList.toggle("hide");
 }
 
