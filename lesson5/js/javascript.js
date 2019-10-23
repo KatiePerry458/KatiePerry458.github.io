e88a18de737e183def2196cb1954152a11860d3f
KatiePerry458 <pie11010@byui.edi>
2019-10-23 11:03:24 -0700

    kp

--- lesson5/js/javascript.js
+++ lesson5/js/javascript.js
@@ -1,15 +1,20 @@
 /* Date Stamp*/
-
+function timeStamp(){
 var timeStamp = Math.floor(Date.now() / 1000);
 var d = new Date();
-
+}
+timeStamp();
 
 /* Banner Announcement*/
-var today = d.getDay();
-if (parseInt == today)
-  case "Friday":
-    $(announcement.style.display = "block").show("hide");
+function banner(){
+  var d = new Date();
+  var today = d.getDay();
+  console.log(today);
+if (today == 5)
+    document.getElementById("announcement").removeAttribute("class","banner");
 }
+banner();
+
 
 /* Responsive Menu*/
 function toggleMenu(){
