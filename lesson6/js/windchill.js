284b67b28996489b0cc744de3cbe589f35ce4e8a
KatiePerry458 <pie11010@byui.edi>
2019-10-28 17:17:49 -0700

    kp

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,7 +1,12 @@
 function windChill(){
-  var temp= ;
-  var speed= ;
+  var temp= parseFloat(document.getElementById("high").innerHTML);
+  var speed= parseFloat(document.getElementById("speed").innerHTML);
 
-  35.74 + (0.6215*temp) - (35.75*s^{0.16}) + 0.4275\:t\:s^{0.16}
+  if (temp >= 50 || speed >=3) {
+  35.74 + (0.6215*temp) - 35.75*(Math.pow(speed, 0.16)) + 0.4275*temp + (Math.pow(speed,0.16));
+  }
 
+  else{
+    NaN
+  }
 }
