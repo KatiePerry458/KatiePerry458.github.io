16bb6e8330e6d7b29182a446ca5bbac32c15c6f4
KatiePerry458 <pie11010@byui.edi>
2019-10-31 11:07:58 -0700

    kp

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,15 +1,18 @@
 function windChill(){
-  var temp = parseFloat(document.getElementById("low").innerHTML);
-  var speed = parseFloat(document.getElementById("speed").innerHTML);
+  var temp = parseFloat(document.getElementById("low").textContent);
+  var speed = parseFloat(document.getElementById("speed").textContent);
+  var calculations;
+console.log(temp, speed);
 
   if (temp >= 50 || speed >=3) {
-  var calculations = 35.74 + (0.6215*temp) - 35.75*(Math.pow(speed, 0.16)) + 0.4275*temp + (Math.pow(speed,0.16));
+  calculations = 35.74 + (0.6215*temp) - 35.75*(Math.pow(speed, 0.16)) + 0.4275*temp + (Math.pow(speed,0.16));
   }
 
   else{
-  var calculations = NaN;
+  calculations = temp;
   }
 
-  document.getElementById("chillcalc").innerHTML = calculations;
+  document.getElementById("chillcalc").innerHTML = calculations.toFixed(1);
 }
 
+windChill();
