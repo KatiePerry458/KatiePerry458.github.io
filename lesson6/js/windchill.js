ade69ba75feac805af064a157deb7e9feb81f8d8
KatiePerry458 <pie11010@byui.edi>
2019-10-30 23:21:17 -0700

    kp

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,12 +1,15 @@
 function windChill(){
-  var temp= parseFloat(document.getElementById("high").innerHTML);
-  var speed= parseFloat(document.getElementById("speed").innerHTML);
+  var temp = parseFloat(document.getElementById("low").innerHTML);
+  var speed = parseFloat(document.getElementById("speed").innerHTML);
 
   if (temp >= 50 || speed >=3) {
-  35.74 + (0.6215*temp) - 35.75*(Math.pow(speed, 0.16)) + 0.4275*temp + (Math.pow(speed,0.16));
+  var calculations = 35.74 + (0.6215*temp) - 35.75*(Math.pow(speed, 0.16)) + 0.4275*temp + (Math.pow(speed,0.16));
   }
 
   else{
-    NaN
+  var calculations = NaN;
   }
+
+  document.getElementById("chillcalc").innerHTML = calculations;
 }
+
