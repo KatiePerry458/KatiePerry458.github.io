35497f63764925c8f10a445dc9e7a0ad8504d616
KatiePerry458 <pie11010@byui.edi>
2019-11-15 14:39:53 -0800

    kp

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -10,19 +10,27 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let image= document.createElement('image');
-      let birthDate= document.createElement('birthDate')
-      let birthPlace= document.createElement('birthPlace')
-
+      let image= document.createElement('img');
+      let birthDate= document.createElement('p')
+      let birthPlace= document.createElement('p')
 
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
       
-      card.appendChild(h2);
-      
       document.querySelector('div.cards').appendChild(card);
       
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      birthDate.textContent = "Date of Birth:" + ' ' + prophets[i].birthDate
+      birthPlace.textContent = "Place of Birth" + ' ' + prophets[i].birthPlace;
       let image = document.createElement('image');
       image.setAttribute('src', prophets[i].imageurl);
+      image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname) + ' ' + prophets[i].order;
+
+      card.appendChild(h2);
+      card.appendChild(image);
+      card.appendChild(birthDate);
+      card.appendChild(birthPlace);
+
+
     }
 
 
