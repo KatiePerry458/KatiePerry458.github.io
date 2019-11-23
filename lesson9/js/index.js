7ba1568a1407287a03243a97560ac3c1bb016e4d
KatiePerry458 <55561445+KatiePerry458@users.noreply.github.com>
2019-11-23 11:36:46 -0800

    kp

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
   .then(function (response) {
@@ -6,20 +6,20 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     //console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++) {
+    const towns = jsonObject['towns'];
+    for (let i = 0; i < towns.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let image = document.createElement('img');
-      let birthDate = document.createElement('p');
-      let birthPlace = document.createElement('p');
+      let image= document.createElement('img');
+      let birthDate= document.createElement('p')
+      let birthPlace= document.createElement('p')
 
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
+      
       document.querySelector('div.cards').appendChild(card);
-
+      
       h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      birthDate.textContent = "Date of Birth:" + ' ' + prophets[i].birthDate;
+      birthDate.textContent = "Date of Birth:" + ' ' + prophets[i].birthDate
       birthPlace.textContent = "Place of Birth" + ' ' + prophets[i].birthPlace;
       image.setAttribute('src', prophets[i].imageurl);
       image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname) + ' ' + prophets[i].order;
