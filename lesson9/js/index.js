c0c755d66d5d814f9652b7e0ab93e166fba9a32c
KatiePerry458 <55561445+KatiePerry458@users.noreply.github.com>
2019-12-02 22:08:15 -0800

    kp

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -5,32 +5,48 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const prophets = jsonObject['prophets'];
-    for (let i = 0; i < prophets.length; i++) {
-      let card = document.createElement('section');
-      let h2 = document.createElement('h2');
-      let photo = document.createElement('photo');
-      let motto = document.createElement('p');
-      let yearFounded = document.createElement('p');
-      let currentPopulation = document.createElement('p');
-      let averageRainfall = document.createElement('p');
+    console.table(jsonObject); 
+    const towns = jsonObject['towns'];
+    console.log(towns);
+    console.log(towns[0]);
+    console.log(towns[0].name);
 
-      h2.textContent = towns[i].name;
-      image.setAttribute('src', towns[i].imageurl);
-      image.setAttribute('alt', towns[i].name + ' ' + prophets[i].lastname) + ' ' + prophets[i].order;
+    for (let i = 0; i < towns.length; i++) {
+      if ( towns[i].name=="Fish Haven" || towns[i].name=="Preston" || towns[i].name=="Soda Springs"){
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let photo = document.createElement('img');
+        let motto = document.createElement('p');
+        let yearFounded = document.createElement('p');
+        let currentPopulation = document.createElement('p');
+        let averageRainfall = document.createElement('p');
+        let events = document.createElement('div');
 
+        name.textContent = towns[i].name;
+        motto.textContent = "Town Motto: " + towns[i].motto;
+        yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
+        currentPopulation.textContent = "Current Population: " + towns[i].currentPopulation;
+        averageRainfall.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+        for (let j=0; j < towns[i].events.length; j++){
+          let event = document.createElement('p');
+          event.textContent=towns[i].events[j];
+          events.appendChild(event);
+        }
 
-      birthPlace.textContent = "Place of Birth:" + ' ' + towns[i].birthplace;
+        photo.setAttribute('src', towns[i].imageurl);
+        photo.setAttribute('alt', towns[i].name);
 
-      card.appendChild(h2);
-      card.appendChild(image);
-      card.appendChild(birthDate);
-      card.appendChild(birthPlace);
-      document.querySelector('div.cards').appendChild(card);
+        card.appendChild(name);
+        card.appendChild(photo);
+        card.appendChild(motto);
+        card.appendChild(yearFounded);
+        card.appendChild(currentPopulation);
+        card.appendChild(averageRainfall);
+        card.appendChild(events);
 
-
+        document.querySelector('div.towns').appendChild(card);
+      }
     }
 
-
   });
+
